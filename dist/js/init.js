436d90c2ca94b3e0d315ad5bd53ee1d3b02bc9e4
Jeffrey777A <44594550+Jeffrey777A@users.noreply.github.com>
2021-01-22 01:18:59 +0000

    .

--- dist/js/init.js
+++ dist/js/init.js
@@ -6,3 +6,15 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+/*$('.hover').mouseover(function(){
+  $('.caption').css('visibility','visible');
+  $('.image_holder').css('opacity','0.6');
+});
+
+$('.hover').mouseout(function(){
+  $('.caption').css('visibility','hidden');
+  $('.image_holder').css('opacity','1');
+});
+*/
